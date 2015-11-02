de04f63f612586786f4ef2c9707044d231ec6172
AZHARULLAH <shariffazharullah@gmail.com>
2015-11-03 02:37:58 +0530

    Final site

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1500);
+        return false;
+      }
+    }
+  });
+});
+;(function(){
+
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
